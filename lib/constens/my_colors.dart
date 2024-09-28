1b03736b33271eecb9ed994b8afdbc531dda9f8d
Mohammed Samy <mom118580@gmail.com>
2024-09-28 00:25:08 -0700

    create login page

---
+++ lib/constens/my_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static const Color myPurple = Color.fromARGB(255, 116, 55, 53);
+  static const Color myGrey = Color.fromARGB(87, 203, 186, 214);
+  static const Color myDeepGrey = Color.fromARGB(255, 121, 121, 121);
+}
--- lib/presenttion/screens/auth/login_page.dart
+++ lib/presenttion/screens/auth/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:education_app/constens/my_colors.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -5,6 +6,65 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: MediaQuery.of(context).size.width / 40,
+        ),
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        color: Colors.white,
+        child: ListView(
+          children: [
+            Column(
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height / 3,
+                ),
+                Text(
+                  "تسجيل الدخول",
+                  style: TextStyle(
+                    fontFamily: "title",
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height / 7,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      "البريد الاكترونى",
+                      style: TextStyle(
+                        fontFamily: "subtitle1",
+                        fontSize: 20,
+                        color: MyColors.myDeepGrey,
+                      ),
+                    ),
+                  ],
+                ),
+                TextFormField(
+                  cursorColor: Colors.black,
+                  textDirection: TextDirection.rtl,
+                  decoration: InputDecoration(
+                    filled: true,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
