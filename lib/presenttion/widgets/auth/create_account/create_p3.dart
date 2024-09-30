a2c6ebed58f58e59476c57bee565053178af7de1
Mohammed Samy <mom118580@gmail.com>
2024-09-30 13:01:55 -0700

    Create Step 3 in Create Page Auth

--- lib/presenttion/widgets/auth/create_account/create_p3.dart
+++ lib/presenttion/widgets/auth/create_account/create_p3.dart
@@ -1,3 +1,4 @@
+import 'package:education_app/constens/my_colors.dart';
 import 'package:education_app/presenttion/widgets/auth/text_form_field_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -8,72 +9,64 @@ class CreateP3 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          textDirection: TextDirection.rtl,
-          children: [
-            TextFormFieldAuth(
-              title: "الاسم الأول",
-              type: "First Name",
-              isLast: false,
-              size: MediaQuery.of(context).size.width / 2.2,
-            ),
-            TextFormFieldAuth(
-              title: "الاسم الأوسط",
-              type: "Second Name",
-              isLast: false,
-              size: MediaQuery.of(context).size.width / 2.2,
-            ),
-          ],
-        ),
-        SizedBox(
-          height: MediaQuery.of(context).size.height / 80,
-        ),
         TextFormFieldAuth(
-          title: "الاسم الأخير",
-          type: "Last Name",
-          isLast: false,
-          size: MediaQuery.of(context).size.width,
-        ),
-        SizedBox(
-          height: MediaQuery.of(context).size.height / 80,
-        ),
-        TextFormFieldAuth(
-          title: "البريد الالكترونى",
-          type: "email",
-          isLast: false,
-          size: MediaQuery.of(context).size.width,
-        ),
-        SizedBox(
-          height: MediaQuery.of(context).size.height / 80,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          textDirection: TextDirection.rtl,
-          children: [
-            TextFormFieldAuth(
-              title: "كلمة المرور",
-              type: "pass",
-              isLast: false,
-              size: MediaQuery.of(context).size.width / 2.2,
-            ),
-            TextFormFieldAuth(
-              title: "تأكيد كلمة المرور",
-              type: "pass",
-              isLast: false,
-              size: MediaQuery.of(context).size.width / 2.2,
-            ),
-          ],
-        ),
-        SizedBox(
-          height: MediaQuery.of(context).size.height / 80,
-        ),
-        TextFormFieldAuth(
-          title: "رقم الموبايل",
+          title: "رقم مكوبايل الأب",
           type: "phone",
           isLast: false,
           size: MediaQuery.of(context).size.width,
         ),
+        SizedBox(
+          height: MediaQuery.of(context).size.height / 40,
+        ),
+        TextFormFieldAuth(
+          title: "رقم موبايل الأم",
+          type: "phone",
+          isLast: false,
+          size: MediaQuery.of(context).size.width,
+        ),
+        SizedBox(
+          height: MediaQuery.of(context).size.height / 40,
+        ),
+        Text(
+          "صورة البطاقة أو شهادة ميلاد الرقم القومى",
+          style: TextStyle(
+            fontFamily: "subtitle2",
+            color: MyColors.myDeepGrey,
+          ),
+        ),
+        SizedBox(
+          height: MediaQuery.of(context).size.height / 40,
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height / 12,
+          decoration: BoxDecoration(
+            color: MyColors.myGrey,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Padding(
+            padding:
+                EdgeInsets.only(right: MediaQuery.of(context).size.width / 60),
+            child: Row(
+              textDirection: TextDirection.rtl,
+              children: [
+                Text(
+                  "    حدد صورة ",
+                  style: TextStyle(
+                    fontFamily: "title",
+                    fontSize: 18,
+                    color: MyColors.myPurple,
+                  ),
+                ),
+                Icon(
+                  Icons.cloud_upload_rounded,
+                  size: MediaQuery.of(context).size.width / 15,
+                  color: MyColors.myPurple,
+                ),
+              ],
+            ),
+          ),
+        )
       ],
     );
   }
