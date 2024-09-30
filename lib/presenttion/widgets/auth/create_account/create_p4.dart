b4e0cee91b96f9ab5b127ec6ee2cd0c49c9a11d6
Mohammed Samy <mom118580@gmail.com>
2024-09-30 10:09:24 -0700

    refactore createw pages

--- lib/presenttion/widgets/auth/create_account/create_p4.dart
+++ lib/presenttion/widgets/auth/create_account/create_p4.dart
@@ -1,4 +1,5 @@
 import 'package:education_app/presenttion/widgets/auth/create_text_fields_page_auth.dart';
+import 'package:education_app/presenttion/widgets/auth/text_form_field_auth.dart';
 import 'package:flutter/material.dart';
 
 class CreateP4 extends StatelessWidget {
@@ -6,6 +7,75 @@ class CreateP4 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CreateTextFieldsPageAuth();
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          textDirection: TextDirection.rtl,
+          children: [
+            TextFormFieldAuth(
+              title: "الاسم الأول",
+              type: "First Name",
+              isLast: false,
+              size: MediaQuery.of(context).size.width / 2.2,
+            ),
+            TextFormFieldAuth(
+              title: "الاسم الأوسط",
+              type: "Second Name",
+              isLast: false,
+              size: MediaQuery.of(context).size.width / 2.2,
+            ),
+          ],
+        ),
+        SizedBox(
+          height: MediaQuery.of(context).size.height / 80,
+        ),
+        TextFormFieldAuth(
+          title: "الاسم الأخير",
+          type: "Last Name",
+          isLast: false,
+          size: MediaQuery.of(context).size.width,
+        ),
+        SizedBox(
+          height: MediaQuery.of(context).size.height / 80,
+        ),
+        TextFormFieldAuth(
+          title: "البريد الالكترونى",
+          type: "email",
+          isLast: false,
+          size: MediaQuery.of(context).size.width,
+        ),
+        SizedBox(
+          height: MediaQuery.of(context).size.height / 80,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          textDirection: TextDirection.rtl,
+          children: [
+            TextFormFieldAuth(
+              title: "كلمة المرور",
+              type: "pass",
+              isLast: false,
+              size: MediaQuery.of(context).size.width / 2.2,
+            ),
+            TextFormFieldAuth(
+              title: "تأكيد كلمة المرور",
+              type: "pass",
+              isLast: false,
+              size: MediaQuery.of(context).size.width / 2.2,
+            ),
+          ],
+        ),
+        SizedBox(
+          height: MediaQuery.of(context).size.height / 80,
+        ),
+        TextFormFieldAuth(
+          title: "رقم الموبايل",
+          type: "phone",
+          isLast: false,
+          size: MediaQuery.of(context).size.width,
+        ),
+      ],
+    );
   }
 }
